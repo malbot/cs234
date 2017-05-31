got simple_driver working, having issues with vanashing gradient with the actor, the critic is learning just fine though. updated how rewards are calculated (also, fixed where s.r [rotation] was used for rewards, instead of s.reward(), this seemed to have caused the vaneshing gradient problem), couple other minor changes'

drivers/action.py
@@ -13,10 +13,10 @@ class Action():
 
     @staticmethod
     def get_action(array, max_delta, max_t):
-        return Action(delta=array[0]*max_delta*2 - max_delta, tr=array[1]*max_t*2 - max_t, tf=array[2]*max_t*2 - max_t)
+        return Action(delta=array[0]*max_delta, tr=array[1]*max_t, tf=array[2]*max_t)
 
     def as_array(self, max_delta, max_t):
-        return np.asarray([self.delta/(max_delta*2) + .5, self.tr/(max_t*2)+.5, self.tf/(max_t*2)+.5])
+        return np.asarray([self.delta/max_delta, self.tr/max_t, self.tf/max_t])
 
     def __call__(self):
         return self.delta, self.tr, self.tf